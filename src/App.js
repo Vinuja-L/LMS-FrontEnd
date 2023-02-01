839af0a27e350d7c18d0b35e27bc2fd29b615ca1
Vinuja-L <lavavinuja@gmail.com>
2023-02-01 15:01:51 +0530

    my part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Header from "./components/common/headings/Header"
-import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css'
-import Home from "./components/home/Home";
-import About from './components/about/About.';
+//import Header from "./components/common/headings/Header"
+//import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import './App.css'
+//import Home from "./components/home/Home";
+//import About from './components/about/About.';
+import Auth from "./pages/Auth";
 
 
 
@@ -11,18 +12,20 @@ import About from './components/about/About.';
 
 function App() {
   return (
-    <>
-      <Router>
-      <Header />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' exact component={About} />      
-      </Switch>
-      </Router>
+   <div className="App">
+     <Auth/>
+    </div>
+  // <>
+  //   <Router>
+  //   <Header />
+  //   <Switch>
+  //     <Route path='/' exact component={Home} />
+  //     <Route path='/about' exact component={About} />      
+  //   </Switch>
+  //   </Router>  
+  // </>
 
-      
-    </>
-  )
+  );
 }
 
 export default App
